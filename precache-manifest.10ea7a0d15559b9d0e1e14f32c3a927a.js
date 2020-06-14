3ff90d9726726e5f8a91a0dfdafc8d38657a25a5
Maxim <s312@tut.by>
2020-06-14 22:57:48 +0300

    Updates

---
+++ precache-manifest.10ea7a0d15559b9d0e1e14f32c3a927a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325e5b99a0dd305204ebd8308e876077",
+    "url": "/DimplomFrontend/index.html"
+  },
+  {
+    "revision": "5071db386d00f9c9c070",
+    "url": "/DimplomFrontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05e7262e77af60f6a409",
+    "url": "/DimplomFrontend/static/js/2.90fdad09.chunk.js"
+  },
+  {
+    "revision": "db10902ae59b21c5a9ff1465ffb29da6",
+    "url": "/DimplomFrontend/static/js/2.90fdad09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5071db386d00f9c9c070",
+    "url": "/DimplomFrontend/static/js/main.e647f25e.chunk.js"
+  },
+  {
+    "revision": "da9e62bb16a2ee9015a5",
+    "url": "/DimplomFrontend/static/js/runtime-main.0dc5e95b.js"
+  }
+]);
